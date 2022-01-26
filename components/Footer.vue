c118f9889bbe7faa6a7b75a7876e2e6ab540f766
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-01-26 22:18:17 +0300

    add footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,11 @@
 <template>
-  <h1>Footer</h1>
+  <footer>
+    <h5>
+      &copy; {{ new Date().getFullYear() }}
+      <span>ComfySloth</span>
+    </h5>
+    <h5>All rights reserved</h5>
+  </footer>
 </template>
 
 <script>
@@ -7,4 +13,25 @@ export default {
   name: 'Footer',
 };
 </script>
-<style scoped></style>
+<style scoped>
+footer{
+  height: 5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: var(--clr-black);
+  text-align: center;
+}
+span {
+  color: var(--clr-primary-5);
+}
+h5 {
+  color: var(--clr-white);
+  margin: 0.1rem;
+
+  font-weight: 400;
+  text-transform: none;
+  line-height: 1.25;
+}
+</style>
