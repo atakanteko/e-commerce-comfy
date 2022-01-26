ae7c430dff7a66404786c81193df969eeee1772b
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-01-26 19:04:24 +0300

    test refactor

--- tests/unit/Navbar.spec.js
+++ tests/unit/Navbar.spec.js
@@ -1,31 +1,28 @@
-import { shallowMount } from '@vue/test-utils';
+import { createLocalVue, mount } from '@vue/test-utils';
 import Navbar from '../../components/Navbar';
 import { links } from '../../utils/constants';
 
-let wrapper = null;
-beforeEach(() => {
-  wrapper = shallowMount(Navbar, {
+function mountComponent() {
+  const localVue = createLocalVue();
+  return mount(Navbar, {
+    localVue,
+    mocks: {
+
+    },
     stubs: {
       NuxtLink: true,
     },
   });
-});
-
-afterEach(() => {
-  wrapper.destroy();
-});
+}
 
 test('Check that the specific image exists', () => {
+  const wrapper = mountComponent();
   const img = wrapper.findAll('#navbar-logo');
   expect(img.length).toBe(1);
 });
 
 test('render list item', async () => {
-  wrapper = shallowMount(Navbar, {
-    stubs: {
-      NuxtLink: true,
-    },
-  });
+  const wrapper = mountComponent();
   await wrapper.setData({ routes: links });
   const routesLength = links.length;
   const linksLength = wrapper.findAll('#navbar-link').length;
