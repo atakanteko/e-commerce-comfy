c19be01f3649da00d622870ed870c3c1855d2da6
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-01-27 22:13:32 +0300

    add loading action/getter/state/mutation

--- store/products_context.js
+++ store/products_context.js
@@ -1,15 +1,25 @@
-export const state = () => ({
+// import { productsUrl } from '../utils/api_urls.js'
+import { SET_LOADING } from './action_types';
 
+export const state = () => ({
+  product_loading: false,
+  product_error: false,
+  products: [],
+  featured_products: [],
 });
 
 export const getters = {
-
+  getProductLoadingStatus: state => state.product_loading,
 };
 
 export const actions = {
-
+  setLoading: ({ commit }, loadingStatus) => {
+    commit(SET_LOADING, loadingStatus);
+  },
 };
 
 export const mutations = {
-
+  [SET_LOADING](state, loadingStatus) {
+    state.product_loading = loadingStatus;
+  },
 };
