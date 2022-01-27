37a73718e16b04911ce4b8d8ba6cd71449442bcf
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-01-27 22:46:11 +0300

    add loading spinner component

---
+++ components/Loading.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="section section-center">
+    <div class="loading" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoadingComponent',
+};
+</script>
+<style scoped></style>
