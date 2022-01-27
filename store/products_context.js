f3809686df0e06bd41750323eb124c513bdc349d
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-01-27 23:56:28 +0300

    fix action type and add await to the dispatch

--- store/products_context.js
+++ store/products_context.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { productsUrl } from '../utils/api_urls.js';
-import { GET_SINGLE_PRODUCT_SUCCESS, SET_LOADING } from './action_types';
+import { GET_PRODUCTS_SUCCESS, SET_LOADING } from './action_types';
 
 export const state = () => ({
   product_loading: false,
@@ -19,16 +19,16 @@ export const actions = {
     commit(SET_LOADING, loadingStatus);
   },
   fetchProducts: async context => {
-    context.dispatch('setLoading', true);
+    await context.dispatch('setLoading', true);
     try {
       const response = await axios.get(productsUrl);
       const products = response.data;
       const featuredProducts = products.filter(item => item.featured === true);
-      context.commit(GET_SINGLE_PRODUCT_SUCCESS, featuredProducts);
-      context.dispatch('setLoading', false);
+      context.commit(GET_PRODUCTS_SUCCESS, featuredProducts);
+      await context.dispatch('setLoading', false);
     } catch (error) {
       console.log(error);
-      context.dispatch('setLoading', true);
+      await context.dispatch('setLoading', true);
     }
   },
 };
@@ -37,7 +37,7 @@ export const mutations = {
   [SET_LOADING](state, loadingStatus) {
     state.product_loading = loadingStatus;
   },
-  [GET_SINGLE_PRODUCT_SUCCESS](state, featuredProducts) {
+  [GET_PRODUCTS_SUCCESS](state, featuredProducts) {
     state.featured_products.push(featuredProducts);
   },
 };
